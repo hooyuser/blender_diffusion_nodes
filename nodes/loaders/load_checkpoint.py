0a9c40308b305020a874181c2573b4c088c9dba5
Derived Cat <hooyuser@outlook.com>
2023-04-22 06:21:31 -0400

    dynamically update checkpoint enum

--- nodes/loaders/load_checkpoint.py
+++ nodes/loaders/load_checkpoint.py
@@ -6,38 +6,24 @@ from pathlib import Path
 
 BASIC_CHECKPOINT_LOADER_NODE_WIDTH = 300
 
-checkpoint_enum_list = [
-    ('0', '0', '0'),
-    ('1', '1', '1'),
-    ('2', '2', '2'),
-    ('3', '3', '3'),
-    ('4', '4', '4'),
-    ('5', '5', '5'),
-    ('6', '6', '6'),
-]
 
+def checkpoint_items_callback(scene, context):
+    # Your code to refresh the nodes goes here
+    addon_prefs = context.preferences.addons["blender_diffusion_nodes"].preferences
+    asset_base_path = Path(addon_prefs.base_dir)
+    checkpoint_path = Path(addon_prefs.checkpoint_dir)
 
-class NODE_OT_refresh(bpy.types.Operator):
-    bl_idname = "node.refresh"
-    bl_label = "Refresh Nodes"
-    bl_description = "Refresh the nodes in the editor"
+    # check if checkpoint path is relative or absolute
+    if not checkpoint_path.is_absolute():
+        checkpoint_path = asset_base_path / checkpoint_path
 
-    def execute(self, context):
-        # Your code to refresh the nodes goes here
-        addon_prefs = context.preferences.addons["blender_diffusion_nodes"].preferences
-        asset_base_path = Path(addon_prefs.base_dir)
-        checkpoint_path = Path(addon_prefs.checkpoint_dir)
+    files = [f for f in checkpoint_path.glob(
+        '**/*') if f.suffix in ('.safetensors', '.ckpt')]
 
-        # check if checkpoint path is relative or absolute
-        if not checkpoint_path.is_absolute():
-            checkpoint_path = asset_base_path / checkpoint_path
+    # sort the files
+    files.sort(key=lambda x: x.stem, reverse=True)
 
-        checkpoint_suffixes = ('.safetensors', '.ckpt')
-        files = [f.stem for f in checkpoint_path.glob(
-            '**/*') if f.suffix in checkpoint_suffixes]
-        print(files)
-
-        return {'FINISHED'}
+    return [(str(i), filename.stem, filename.name) for i, filename in enumerate(files)]
 
 
 class LoadCheckpointNode(bpy.types.Node):
@@ -47,31 +33,30 @@ class LoadCheckpointNode(bpy.types.Node):
     bl_label = 'Load Checkpoint'
     bl_icon = 'TEXT'
 
-    # def update_prop(self, context):
-    #     for link in self.outputs[0].links:
-    #         link.to_socket.default_value = self.value
+    checkpoint_enum_list = [
+        ('0', '0', '0'),
+        ('1', '1', '1'),
+        ('2', '2', '2'),
+        ('3', '3', '3'),
+        ('4', '4', '4'),
+        ('5', '5', '5'),
+        ('6', '6', '6'),
+    ]
 
     checkpoint_enum: bpy.props.EnumProperty(
-        items=checkpoint_enum_list,
+        items=checkpoint_items_callback,
         name='Checkpoint',
-        description='Checkpoint to load',
-        default='0',
+        description='Checkpoint to load'
     )
 
     def init(self, context):
         BaseNode.base_init(self, context)
-        #self.index = -3
         self.outputs.new('DiffusionSocketModel', "Model")
         self.outputs.new('DiffusionSocketCLIP', "CLIP")
         self.outputs.new('DiffusionSocketVAE', "VAE")
         self.width = BASIC_CHECKPOINT_LOADER_NODE_WIDTH
 
     def draw_buttons(self, context, layout):
-        # create a slider for int values
         # add row
         row = layout.row()
         row.prop(self, 'checkpoint_enum', text='Checkpoint')
-
-        # add refresh button to row
-        row.operator('node.refresh', text='', icon='FILE_REFRESH')
-        # define refresh button operator
